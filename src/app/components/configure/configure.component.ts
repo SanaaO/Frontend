e3c0b9c5c67f219fb9b64d244d88e27fa12a6f41
SanaaO <saana.oueslati@gmail.com>
2021-11-12 20:03:30 +0100

    List all 3D objects stored database

--- src/app/components/configure/configure.component.ts
+++ src/app/components/configure/configure.component.ts
@@ -1,15 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { Shape } from 'src/app/models/shape';
+import { ShapeServiceService } from 'src/app/services/shape-service.service';
 
 @Component({
   selector: 'app-configure',
   templateUrl: './configure.component.html',
-  styleUrls: ['./configure.component.css']
+  styleUrls: ['./configure.component.css'],
 })
-export class ConfigureComponent implements OnInit {
+export class ConfigureComponent implements OnInit, OnDestroy {
+  shapeSub!: Subscription;
+  shapes: Shape[] = [];
 
-  constructor() { }
+  constructor(private shapeService: ShapeServiceService) {}
 
   ngOnInit(): void {
+    this.shapeSub = this.shapeService.getAllShapes().subscribe(
+      (result) => {
+        this.shapes = result.data;
+      },
+      (error) => {
+        console.log('Error message : ' + error.error);
+      }
+    );
   }
 
+  ngOnDestroy() {
+    this.shapeSub.unsubscribe();
+  }
 }
